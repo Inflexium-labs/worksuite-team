90d7506f3fa5af6d3202d7e65583d89f065d9485
hmimeee <hmimeee@gmail.com>
2021-06-15 16:11:48 -0300

    Language updated

---
+++ Resources/lang/es/app.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+    'team' => 'Equipo',
+    'createTeam' => 'Crear Equipo',
+    'editTeam' => 'Editar Equipo',
+    'teamView' => 'Ver Equipo',
+    'members' => 'Miemrbos',
+    'teamInformation' => 'Informacion de Equipo',
+    'teamName' => 'Nombre de Equipo',
+    'since' => 'Desde',
+    'teamLeader' => 'Lider del Equipo',
+    'teamMembers' => 'Miembros del Equipo',
+    'updateStatus' => 'Actualizar Estado',
+    'you' => 'YOU',
+
+    'messages' => array(
+        'createSuccess' => 'Equipo creado exitosamente',
+        'updateSuccess' => 'Equipo actualizado exitosamente',
+        'deleteSuccess' => 'Equipo eliminado exitosamente',
+        'statusUpdateSuccess' => 'Estado del Equipo actualizado exitosamente',
+        'memberAddSuccess' => 'Miemrbo agregado exitosamente',
+        'memberRemoveSuccess' => 'Miemrbo eliminado exitosamente',
+        'leaderUpdateSuccess' => 'Lider del Equipo actualizado exitosamente',
+    )
+);
---
+++ Resources/lang/sw/app.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+    'team' => 'Organización Laboral',
+    'createTeam' => 'Crear Organización',
+    'editTeam' => 'Editar Organización',
+    'teamView' => 'Ver Organización',
+    'members' => 'Miembros',
+    'teamInformation' => 'Informacion de la Organización',
+    'teamName' => 'Nombre de la Organización',
+    'since' => 'Desde',
+    'teamLeader' => 'Lider de la Sucursal',
+    'teamMembers' => 'Miembros de la Sucursal',
+    'updateStatus' => 'Actualizar Estado',
+    'you' => 'YOU',
+
+    'messages' => array(
+        'createSuccess' => 'Organización Laboral realizada exitosamente',
+        'updateSuccess' => 'Organización Laboral actualizada exitosamente',
+        'deleteSuccess' => 'Organización Laboral eliminada exitosamente',
+        'statusUpdateSuccess' => 'Estado de la Organización Laboral actualizado exitosamente',
+        'memberAddSuccess' => 'Miembro agregado exitosamente',
+        'memberRemoveSuccess' => 'Miembro eliminado exitosamente',
+        'leaderUpdateSuccess' => 'Lider de la Sucursal actualizado exitosamente',
+    )
+);
